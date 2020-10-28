b6cdc5d7267e7546a3778577938e61af5c0e3080
Idanatiya <idanatiya122@gmail.com>
2020-10-28 16:27:04 +0200

    added map

--- js/services/map-service.js
+++ js/services/map-service.js
@@ -1,43 +1,22 @@
 'use strict';
+var gMap;
 
 
-export const mapService = {
-    iAmAMap,
-}
 
 // test if export works: 
 function iAmAMap() {
     console.log(' MAPSSSSSSS!')
 }
 
-export function initMap(lat = 32.0749831, lng = 34.9120554) {
-    console.log('InitMap');
-    return _connectGoogleApi()
-        .then(() => {
-            console.log('google available');
-            gMap = new google.maps.Map(
-                document.querySelector('#map'), {
-                    center: { lat, lng },
-                    zoom: 15
-                })
-            console.log('Map!', gMap);
-        })
-}
+var locs = [{ lat: 11.22, lng: 22.11 }]
 
-
-
-function _connectGoogleApi() {
-    if (window.google) return Promise.resolve()
-    const API_KEY = 'AIzaSyCQigCXfm-p5j0gFc2LVoLLhd1EW1gXWTo'; //TODO: Enter your API Key
-    var elGoogleApi = document.createElement('script');
-    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
-    elGoogleApi.async = true;
-    document.querySelector('#map').innerHTML = elGoogleApi;
-
-    document.body.append(elGoogleApi);
-
+function getLocs() {
     return new Promise((resolve, reject) => {
-        elGoogleApi.onload = resolve;
-        elGoogleApi.onerror = () => reject('Google script failed to load')
-    })
+        setTimeout(() => {
+            resolve(locs);
+        }, 2000)
+    });
+}
+export const mapService = {
+    getLocs
 }
