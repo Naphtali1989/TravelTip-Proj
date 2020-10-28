8825b74a690b4c9074e46fe9a65f8637b1e319c8
Idanatiya <idanatiya122@gmail.com>
2020-10-28 15:47:34 +0200

    added class to form

--- js/services/map-service.js
+++ js/services/map-service.js
@@ -9,3 +9,35 @@ export const mapService = {
 function iAmAMap() {
     console.log(' MAPSSSSSSS!')
 }
+
+export function initMap(lat = 32.0749831, lng = 34.9120554) {
+    console.log('InitMap');
+    return _connectGoogleApi()
+        .then(() => {
+            console.log('google available');
+            gMap = new google.maps.Map(
+                document.querySelector('#map'), {
+                    center: { lat, lng },
+                    zoom: 15
+                })
+            console.log('Map!', gMap);
+        })
+}
+
+
+
+function _connectGoogleApi() {
+    if (window.google) return Promise.resolve()
+    const API_KEY = 'AIzaSyCQigCXfm-p5j0gFc2LVoLLhd1EW1gXWTo'; //TODO: Enter your API Key
+    var elGoogleApi = document.createElement('script');
+    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+    elGoogleApi.async = true;
+    document.querySelector('#map').innerHTML = elGoogleApi;
+
+    document.body.append(elGoogleApi);
+
+    return new Promise((resolve, reject) => {
+        elGoogleApi.onload = resolve;
+        elGoogleApi.onerror = () => reject('Google script failed to load')
+    })
+}
